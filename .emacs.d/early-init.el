d789c81740cf59adf775550525eec7275961db8c
LeanMC <gmclean@gmail.com>
2025-01-15 20:54:42 -0800

    new mode line
    some more de-cluttering, but mostly a still-in-progress but greatly
    improved modeline

--- .emacs.d/early-init.el
+++
@@ -1,7 +0,0 @@
-(require 'package)
-(require 'use-package-ensure)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(setopt use-package-always-ensure t)
-(unless package-archive-contents
-  (package-refresh-contents))
